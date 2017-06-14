dc46a6f356c58e17b6eec5a89d0918d21bc49a0b
Fonky44 <fonky44@gmail.com>
2017-06-14 15:42:12 +0200

    updated

--- UI.py
+++ UI.py
@@ -3,8 +3,11 @@ import Main
 
 root = tk.Tk()
 
+Frame = tk.Frame(root, width=500, height=500)
+Name = tk.Label(root, text = "Name")
 
-
-
+Frame.grid_propagate(0)
+Frame.grid()
+Name.grid()
 
 root.mainloop()
